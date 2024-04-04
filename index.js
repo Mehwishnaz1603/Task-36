aeae8ac0bee318fd5452c1bbb7ce83768871cee2
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 18:14:20 +0500

    Task no 36

---
+++ index.js
@@ -0,0 +1,11 @@
+///Task # 36 ///
+//T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message
+// that should be printed on the shirt. The function should print a sentence summarizing the 
+//size of the shirt and the message printed on it. Call the function.
+function make_shirt(size, message) {
+    console.log(`The shirt size should be: ${size}, and must be printed with: ${message}`);
+}
+make_shirt("Small", "<\My Life My Rules\>");
+make_shirt("Medium", "<\I am a Codinggirl\>");
+make_shirt("Large", "<\I love codimg\>");
+export {};
